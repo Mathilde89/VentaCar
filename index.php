00a2e58919ae43bb636e99f9282c346b982b85dd
nicolas.tamareg <ntamareg13@gmail.com>
2022-11-08 15:48:17 +0100

    master2

--- index.php
+++ index.php
@@ -39,14 +39,3 @@ var_dump($listcars)
 
 </body>
 </html>
-=======
-
-//Reccupère les informations de l'utilisateur connecté
-session_start();
- $id_session = session_id();
- var_dump($_COOKIE['PHPSESSID']);
- var_dump($id_session);
-var_dump($_SESSION);
-
- ?>
->>>>>>> origin/math
